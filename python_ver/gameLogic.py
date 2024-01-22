ec16aa811b0301c7bd719582591c776f4671b82f
Clownmeat <suckmeoff@yahoo.com>
2024-01-22 16:31:18 -0500

    removed unwanted space

--- python_ver/gameLogic.py
+++ python_ver/gameLogic.py
@@ -9,7 +9,6 @@ person = Player(name='cool guy', health=100, weapon=1, armour=0)
 
 for n in range(10):
     person.move_forward()
-    
     while(person.location[0] < 3):
         if(person.location[1] > 3):
             person.location[1] = random.randint(0, 3)
