2485dbc845d3cc1063732207f22b70e19c26c438
Clownmeat <suckmeoff@yahoo.com>
2024-01-26 03:15:59 -0500

    added tk inter to game gameLogic

--- python_ver/gameLogic.py
+++ python_ver/gameLogic.py
@@ -1,5 +1,25 @@
 from controllers.user import Player
-import random
+from tkinter import *
+
+window = Tk(screenName='Forever Maze')
+window.title("Forever Maze")
+window.minsize(width=1200, height=720)
+window.config(padx=100, pady=100)
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
 
 player = Player(name='cool guy', health=100, weapon=1, armour=0)
 
