ca7fff0608cbf6464fb05d86265f9415d6b541de
Clownmeat <suckmeoff@yahoo.com>
2024-01-29 19:56:01 -0500

    added window elements for map, text box, and item card.
    added dungeonMaster in controllers

--- python_ver/gameLogic.py
+++ python_ver/gameLogic.py
@@ -4,14 +4,22 @@ from tkinter import *
 window = Tk()
 window.title("Forever Maze")
 window.minsize(width=480, height=480)
-window.config(padx=10, pady=10, bg="black")
-
+window.config(bg="black")
+window.geometry = ('300x300')
 
 
 
 gameMap = Canvas()
-gameMap.config(bg='black', )
-gameMap.grid(row=0, column=1)
+gameMap.config(bg='blue', width=250)
+gameMap.grid(row=0, column=1, padx=10, pady=10, columnspan=1, sticky=N)
+
+
+
+itemText = Canvas()
+itemText.config(bg='white', width=200, height=200)
+itemText.grid(row=0, column=0, padx=40, pady=40, columnspan=2, sticky='w')
+
+
 
 player = Player(name='cool guy', health=100, weapon=1, armour=0)
 
@@ -22,7 +30,7 @@ def startGame():
     global response
     question = Entry(width=100)
     question.config(textvariable='What is your choice?')
-    question.grid(row=1, column=1)
+    question.grid(row=1, column=0, columnspan=3, padx=20, sticky=S, pady=20)
     
     
 
